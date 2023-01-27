387704825a7bf64c1895ab0b49270f216b607f46
Shoaibv2 <skrj1337@gmail.com>
2023-01-27 23:42:31 +0500

    Add Testimonials Section

--- pages/index.js
+++ pages/index.js
@@ -125,6 +125,39 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <section className="text-gray-600">
+        <div className="container px-5 py-16 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302" />
+                <p className="leading-relaxed">“Having chemistry with your partners is key, and we’ve definitely found that as well as a very strategic mind—in our account team. We simply can’t envision our marketing mix without their content strategy and media relations savvy!”</p>
+                <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                <p className="text-gray-500">Senior Product Designer</p>
+              </div>
+            </div>
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/300x300" />
+                <p className="leading-relaxed">“Plicio was engaged and helpful throughout the entire decision-making process, answering all of our questions and concerns. Once the Interdependence team came on board we saw almost instant results, with a steady flow of features and interest from media and influencers.”</p>
+                <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">ALPER KAMU</h2>
+                <p className="text-gray-500">UI Develeoper</p>
+              </div>
+            </div>
+            <div className="lg:w-1/3 lg:mb-0 p-4">
+              <div className="h-full text-center">
+                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/305x305" />
+                <p className="leading-relaxed">“Having a partner that understands the intricacies of our industry has been an invaluable asset for our team. From standard releases to thought leadership and community activations, the Interdependence team has helped shape and promote our messaging with salient attention to brand position and values.”</p>
+                <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">HENRY LETHAM</h2>
+                <p className="text-gray-500">CTO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
