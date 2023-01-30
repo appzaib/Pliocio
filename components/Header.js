df5242c271c458bd2d3b1a996fa11cdf78a49aa5
Shoaibv2 <skrj1337@gmail.com>
2023-01-31 01:02:55 +0500

    Add About Us Page

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,12 @@ function Header() {
         <Link href={"/"} className="flex title-font font-medium items-center text-sky-900 mb-4 md:mb-0">
           <img src="logo.png" height="80px" width="180px" alt="" />
         </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-medium">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href={"#services"} className="mr-8 hover:text-cyan-500">Services</Link>
-          <Link href={""} className="mr-8 hover:text-cyan-500">About Us</Link>
+          <Link href={"about"} className="mr-8 hover:text-cyan-500">About Us</Link>
           <Link href={""} className="mr-8 hover:text-cyan-500">Work</Link>
         </nav>
-        <Link href={"#contact"} className="font-medium inline-flex items-center bg-cyan-400 border-0 py-1 px-3 focus:outline-none hover:bg-cyan-500 rounded text-base mt-4 md:mt-0">Talk To Us
+        <Link href={"#contact"} className="inline-flex items-center bg-cyan-400 border-0 py-1 px-3 focus:outline-none hover:bg-cyan-500 rounded text-base mt-4 md:mt-0">Talk To Us
         </Link>
       </div>
     </header>
