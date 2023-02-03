e4a1cd94e6fd37695a1f282c5fe6f6a1a5919c4b
Shoaibv2 <skrj1337@gmail.com>
2023-02-03 19:03:04 +0500

    Remove Bg  of Testimonials Section

--- pages/index.js
+++ pages/index.js
@@ -30,17 +30,19 @@ export default function Home() {
 
       <section class="py-4">
         <div class="container mx-auto">
-          <h2 class="text-3xl font-bold text-center mb-6">Our Services</h2>
+          <h2 class="text-3xl font-bold text-center mb-6 text-sky-800">
+            Our Services
+          </h2>
           <div class="flex flex-wrap -mx-4">
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaMicrophoneAlt className="text-gray-600 text-3xl" />
+                  <FaMicrophoneAlt className="text-sky-600 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Media Relations
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We help clients establish and maintain strong relationships
                   with the media to ensure their messages are heard and their
                   stories are told.
@@ -50,12 +52,12 @@ export default function Home() {
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaMedkit className="text-gray-500 text-3xl" />
+                  <FaMedkit className="text-sky-500 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Crisis Management
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We provide expert guidance and support to clients facing a
                   crisis, helping them to manage the situation and minimize
                   negative impact on their reputation.
@@ -65,12 +67,12 @@ export default function Home() {
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaCalendarAlt className="text-gray-500 text-3xl" />
+                  <FaCalendarAlt className="text-sky-500 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Event Planning
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We specialize in creating and executing events that captivate
                   and engage audiences, build brand awareness and drive results
                   for clients.
@@ -80,12 +82,12 @@ export default function Home() {
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaFileAlt className="text-gray-500 text-3xl" />
+                  <FaFileAlt className="text-sky-500 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Content Creation
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We develop high-quality, engaging content across various
                   formats and platforms to help clients communicate their
                   messages and build their brand.
@@ -95,12 +97,12 @@ export default function Home() {
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaUsers className="text-gray-500 text-3xl" />
+                  <FaUsers className="text-sky-500 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Influencer Marketing
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We help clients connect with relevant influencers and build
                   lasting relationships that increase their brand reach and
                   credibility.
@@ -110,12 +112,12 @@ export default function Home() {
             <div class="w-full md:w-1/3 px-4 mb-8">
               <div class="bg-white h-64 rounded-lg shadow-lg p-8 text-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                 <div class="flex items-center justify-center h-12 w-12 mx-auto mb-2">
-                  <FaTwitter className="text-gray-500 text-3xl" />
+                  <FaTwitter className="text-sky-500 text-3xl" />
                 </div>
-                <h3 class="font-bold text-xl text-gray-800 mb-4">
+                <h3 class="font-bold text-xl text-sky-800 mb-4">
                   Social Media Management
                 </h3>
-                <p class="text-gray-700 text-base">
+                <p class="text-sky-700 text-base">
                   We help clients establish and grow their presence on social
                   media, creating and sharing content that resonates with their
                   audience and drives engagement.
@@ -138,7 +140,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Dramatically boost your authority and market leadership in
                   your industry with prospects and peers
                 </h2>
@@ -149,7 +151,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Raise awareness on new products and services before prospects
                   even know to search for them
                 </h2>
@@ -160,7 +162,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Increase social proof with excerpts from high profile
                   publications
                 </h2>
@@ -171,7 +173,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Attract more visitors to your website when readers come across
                   your story and within search engines
                 </h2>
@@ -182,7 +184,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Improve your search engine rankings with article backlinks
                 </h2>
               </div>
@@ -192,7 +194,7 @@ export default function Home() {
                 <IoMdCheckmarkCircleOutline />
               </div>
               <div className="flex-grow">
-                <h2 className="text-white text-base font-medium">
+                <h2 className="text-white text-[17px] font-medium">
                   Stand out from competitors, demonstrate expertise and thought
                   leadership.
                 </h2>
@@ -201,13 +203,14 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="text-white bg-sky-600">
-        <div className="container px-5 py-8 mx-auto">
-          <h2 className="text-center text-3xl font-semibold py-8">
+
+      <section>
+        <div className="container px-5 py-14 mx-auto">
+          <h2 className="text-center text-3xl font-semibold pb-8">
             Testimonials
           </h2>
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-8">
               <div className="h-full text-center">
                 <p className="leading-relaxed">
                   “Having chemistry with your partners is key, and we’ve
@@ -215,14 +218,14 @@ export default function Home() {
                   account team. We simply can’t envision our marketing mix
                   without their content strategy and media relations savvy!”
                 </p>
-                <span className="inline-block h-1 w-10 rounded bg-cyan-500 mt-6 mb-4"></span>
-                <h2 className="text-violet-900 font-medium title-font tracking-wider text-sm">
+                <span className="inline-block h-1 w-10 rounded bg-sky-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
                   HOLDEN CAULFIELD
                 </h2>
-                <p className="text-white">Senior Product Designer</p>
+                <p className="">Senior Product Designer</p>
               </div>
             </div>
-            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-8">
               <div className="h-full text-center">
                 <p className="leading-relaxed">
                   “Plicio was engaged and helpful throughout the entire
@@ -231,14 +234,14 @@ export default function Home() {
                   almost instant results, with a steady flow of features and
                   interest from media and influencers.”
                 </p>
-                <span className="inline-block h-1 w-10 rounded bg-cyan-500 mt-6 mb-4"></span>
-                <h2 className="text-violet-900 font-medium title-font tracking-wider text-sm">
+                <span className="inline-block h-1 w-10 rounded bg-sky-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
                   ALPER KAMU
                 </h2>
-                <p className="text-white">UI Develeoper</p>
+                <p className="">UI Develeoper</p>
               </div>
             </div>
-            <div className="lg:w-1/3 lg:mb-0 p-4">
+            <div className="lg:w-1/3 lg:mb-0 p-8">
               <div className="h-full text-center">
                 <p className="leading-relaxed">
                   “Having a partner that understands the intricacies of our
@@ -248,18 +251,18 @@ export default function Home() {
                   promote our messaging with salient attention to brand position
                   and values.”
                 </p>
-                <span className="inline-block h-1 w-10 rounded bg-cyan-500 mt-6 mb-4"></span>
-                <h2 className="text-violet-900 font-medium title-font tracking-wider text-sm">
+                <span className="inline-block h-1 w-10 rounded bg-sky-500 mt-6 mb-4"></span>
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
                   HENRY LETHAM
                 </h2>
-                <p className="text-white">CTO</p>
+                <p className="">CTO</p>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section id="contact" className="text-white bg-cyan-800 relative">
-        <div className="container px-5 py-8 mx-auto">
+      <section id="contact" className="text-white bg-sky-800 relative">
+        <div className="container px-5 py-6 mx-auto">
           <div className="flex flex-col text-center w-full mb-8">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-1 text-white">
               Interested in working with us? <br /> Drop us a line.
@@ -279,7 +282,7 @@ export default function Home() {
                     type="text"
                     id="name"
                     name="name"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-cyan-100 focus:ring-2 focus:ring-cyan-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-sky-500 focus:bg-sky-100 focus:ring-2 focus:ring-sky-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
               </div>
@@ -295,7 +298,7 @@ export default function Home() {
                     type="email"
                     id="email"
                     name="email"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-cyan-100 focus:ring-2 focus:ring-cyan-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-sky-500 focus:bg-sky-100 focus:ring-2 focus:ring-sky-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
               </div>
@@ -311,7 +314,7 @@ export default function Home() {
                     type="tel"
                     id="email"
                     name="email"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-cyan-100 focus:ring-2 focus:ring-cyan-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-sky-500 focus:bg-sky-100 focus:ring-2 focus:ring-sky-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
               </div>
@@ -326,12 +329,12 @@ export default function Home() {
                   <textarea
                     id="message"
                     name="message"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-cyan-100 focus:ring-2 focus:ring-cyan-200 h-16 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-sky-500 focus:bg-sky-100 focus:ring-2 focus:ring-sky-200 h-16 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                   ></textarea>
                 </div>
               </div>
               <div className="p-2 w-full">
-                <button className="flex mx-auto text-white bg-cyan-500 border-0 py-2 px-8 focus:outline-none hover:bg-cyan-600 rounded text-lg">
+                <button className="flex mx-auto text-white bg-sky-500 border-0 py-2 px-8 focus:outline-none hover:bg-sky-600 rounded text-lg">
                   GET STARTED
                 </button>
               </div>
