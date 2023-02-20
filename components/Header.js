be457fc3b991008dc7c3f242bf7e2fbdd86bc894
Shoaibv2 <skrj1337@gmail.com>
2023-02-21 01:56:20 +0500

    Change Bg-Color

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="text-white sticky top-0 z-10 bg-[#849de2]">
+    <header className="text-white sticky top-0 z-10 bg-[#18206F]">
       <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
         <Link
           href={"/"}
@@ -11,19 +11,19 @@ function Header() {
           <img src="logo.png" height="60px" width="160px" alt="" />
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <Link href={"/services"} className="mr-8 hover:text-[#b1c2f2]">
+          <Link href={"/services"} className="mr-8">
             Services
           </Link>
-          <Link href={"/about"} className="mr-8 hover:text-[#b1c2f2]">
+          <Link href={"/about"} className="mr-8">
             About Us
           </Link>
-          <Link href={""} className="mr-8 hover:text-[#b1c2f2]">
+          <Link href={""} className="mr-8">
             Work
           </Link>
         </nav>
         <Link
           href={"/contact"}
-          className="inline-flex items-center bg-sky-400 border-0 py-1 px-3 focus:outline-none hover:bg-sky-500 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-[#182460] border-0 py-1 px-3 focus:outline-none hover:bg-[#18265B] rounded text-base mt-4 md:mt-0"
         >
           Talk To Us
         </Link>
